32296cb87043f95b402ef808199e4c018fbd0d73
Administrator <48503653+Scrince@users.noreply.github.com>
2026-07-05 22:22:34 -0400

    Create drive_enumerator.h

---
+++ src/platform/drive_enumerator.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "platform/drive_info.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace datascythe {
+
+class IDriveEnumerator {
+public:
+    virtual ~IDriveEnumerator() = default;
+    virtual std::vector<DriveInfo> enumerate(std::string& error_out) = 0;
+};
+
+std::unique_ptr<IDriveEnumerator> create_drive_enumerator();
+
+}  // namespace datascythe
