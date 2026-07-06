2a6920fd39f28a7df92d954d3c686ded3a73ff3d
Administrator <48503653+Scrince@users.noreply.github.com>
2026-07-05 22:20:30 -0400

    Create confirmation_dialog.h

---
+++ apps/gui/confirmation_dialog.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "platform/drive_info.h"
+
+#include <QDialog>
+#include <QString>
+
+class QLineEdit;
+class QLabel;
+
+namespace datascythe::gui {
+
+class ConfirmationDialog : public QDialog {
+    Q_OBJECT
+
+public:
+    ConfirmationDialog(const DriveInfo& drive, const QString& operation_title,
+                       const QString& operation_description, QWidget* parent = nullptr);
+
+    /// Generic confirmation for file/folder targets.
+    ConfirmationDialog(const QString& target_path, const QString& confirmation_phrase,
+                       const QString& operation_title, const QString& operation_description,
+                       QWidget* parent = nullptr);
+
+    bool confirmed() const { return confirmed_; }
+
+private slots:
+    void on_accept();
+
+private:
+    bool confirmed_ = false;
+    QString required_phrase_;
+    QLineEdit* phrase_edit_ = nullptr;
+    QLabel* warning_label_ = nullptr;
+};
+
+}  // namespace datascythe::gui
