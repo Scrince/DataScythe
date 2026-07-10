98e4f143783f62f21e7378fe65f2b93da766af38
Administrator <48503653+Scrince@users.noreply.github.com>
2026-07-09 20:13:40 -0400

    Update DeployWindows.cmake

--- cmake/DeployWindows.cmake
+++ cmake/DeployWindows.cmake
@@ -1,4 +1,4 @@
-# Bundle MinGW runtime DLLs next to a Windows executable target.
+
 function(datascythe_deploy_mingw target_name)
     if(NOT WIN32 OR NOT MINGW)
         return()
@@ -25,11 +25,36 @@ function(datascythe_deploy_qt target_name)
         return()
     endif()
 
+    if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+        set(DATASCYTHE_DXC_ARCH x64)
+    else()
+        set(DATASCYTHE_DXC_ARCH x86)
+    endif()
+
+    set(DATASCYTHE_DXC_SEARCH_DIRS
+        "C:/Program Files (x86)/Windows Kits/10/Redist/D3D/${DATASCYTHE_DXC_ARCH}"
+        "C:/Program Files (x86)/Windows Kits/10/bin/10.0.26100.0/${DATASCYTHE_DXC_ARCH}"
+        "C:/Windows/System32/Microsoft-Edge-WebView"
+    )
+
+    find_file(DATASCYTHE_DXCOMPILER_DLL dxcompiler.dll PATHS ${DATASCYTHE_DXC_SEARCH_DIRS} NO_DEFAULT_PATH)
+    find_file(DATASCYTHE_DXIL_DLL dxil.dll PATHS ${DATASCYTHE_DXC_SEARCH_DIRS} NO_DEFAULT_PATH)
+
+    foreach(dxc_dll IN ITEMS "${DATASCYTHE_DXCOMPILER_DLL}" "${DATASCYTHE_DXIL_DLL}")
+        if(dxc_dll AND EXISTS "${dxc_dll}")
+            add_custom_command(TARGET ${target_name} POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                        "${dxc_dll}"
+                        "$<TARGET_FILE_DIR:${target_name}>"
+                COMMENT "Deploy ${dxc_dll} for ${target_name}")
+        endif()
+    endforeach()
+
     find_program(WINDEPLOYQT_EXECUTABLE windeployqt
                  HINTS "${Qt6_DIR}/../../../bin" "${Qt5_DIR}/../../../bin")
     if(WINDEPLOYQT_EXECUTABLE)
         add_custom_command(TARGET ${target_name} POST_BUILD
-            COMMAND "${WINDEPLOYQT_EXECUTABLE}" "$<TARGET_FILE:${target_name}>"
+            COMMAND "${WINDEPLOYQT_EXECUTABLE}" --no-system-dxc-compiler "$<TARGET_FILE:${target_name}>"
             COMMENT "Running windeployqt for ${target_name}")
     endif()
 endfunction()
